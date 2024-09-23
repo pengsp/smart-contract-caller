8a526397bf3a66933c67751528a552e88534f3e7
pengsp <vip.sp.peng@gmail.com>
2024-09-24 01:47:45 +0800

    style: add background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <>
       <Nav />
-      <div className=" h-[calc(100vh-100px)] min-h-[900px] bg-gray-100 p-4 flex flex-col">
-        <Card rootClassName="bg-white flex flex-col gap-4 h-full">
+      <div className=" h-[calc(100vh-100px)] min-h-[900px] bg-gray-100 p-4 flex flex-col bg-[url('/images/bg.jpg')] bg-no-repeat bg-top">
+        <Card rootClassName="bg-white/90 flex flex-col gap-4 h-full">
           <Contracts />
           <OperationPanel />
         </Card>
