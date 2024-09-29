877a9987685dc93ef7a60f39c383c6d596e4d232
pengsp <vip.sp.peng@gmail.com>
2024-09-30 01:17:20 +0800

    feat: update language icon

--- src/app/componets/Icons/IconTranslate.tsx
+++ src/app/componets/Icons/IconTranslate.tsx
@@ -1,11 +1,19 @@
 export function IconTranslate(props: { className?: string }) {
-    return (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props} >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <path d="M4 5h7" />
-        <path d="M7 4c0 4.846 0 7 .5 8" />
-        <path d="M10 8.5c0 2.286 -2 4.5 -3.5 4.5s-2.5 -1.135 -2.5 -2c0 -2 1 -3 3 -3s5 .57 5 2.857c0 1.524 -.667 2.571 -2 3.143" />
-        <path d="M12 20l4 -9l4 9" />
-        <path d="M19.1 18h-6.2" />
+    return (<svg width="24px" height="24px" viewBox="0 0 21 21" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <g fill="none" fill-rule="evenodd" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" transform="translate(2 2)">
+            <path d="m16.5 8.5v-6c0-1.1045695-.8954305-2-2-2h-6c-1.1045695 0-2 .8954305-2 2v6c0 1.1045695.8954305 2 2 2h6c1.1045695 0 2-.8954305 2-2z" />
+            <path d="m4.5 6.50344846h-2.00001427c-1.1045695 0-2 .8954305-2 2v5.99943324c0 1.1045695.8954305 2 2 2h.00345627l6.00001428-.0103718c1.10321833-.0019065 1.99654372-.8967771 1.99654372-1.999997v-1.9925129" />
+            <g transform="translate(2.502 9.5)">
+                <path d="m2.998 1.003h-3" />
+                <path d="m4.49841597 2.5c-.33333333.33333333-.66666667.66666667-1 1s-1.16666667.83333333-2.5 1.5" />
+                <path d="m.99841597 1.00316806c.33333333 1.16613866.83333333 1.99894398 1.5 2.49841597s1.5.99894398 2.5 1.49841597" />
+            </g>
+            <g transform="translate(8.5 2.5)">
+                <path d="m3 0-3 6" />
+                <path d="m3 0 3 6" />
+                <path d="m3 2v4" transform="matrix(0 1 -1 0 7 1)" />
+            </g>
+        </g>
     </svg>
     )
 }
--- src/app/componets/Layout/LanguageSwitch/index.tsx
+++ src/app/componets/Layout/LanguageSwitch/index.tsx
@@ -1,5 +1,5 @@
 
-import { startTransition } from "react";
+import { startTransition, useState } from "react";
 import { useLocale } from "next-intl";
 import { Button, Popover } from "antd";
 import { setUserLocale } from "@/i18n/service";
@@ -9,6 +9,7 @@ import { langsMap } from "@/i18n/config"
 import { IconTranslate } from "../../Icons";
 
 export default function LanguageSwitch() {
+    const [open, setOpen] = useState(false);
 
     const lang = useLocale();
 
@@ -21,12 +22,16 @@ export default function LanguageSwitch() {
 
     const switchLang = (lang: string) => {
         startTransition(() => {
+            setOpen(false);
             setUserLocale(lang);
         });
     }
+    const handleOpenChange = (newOpen: boolean) => {
+        setOpen(newOpen);
+    };
     return (<>
-        <Popover placement="bottomRight" content={topLangsDom} trigger="click" >
-            <div className="cursor-pointer  rounded p-1 px-2 bg-gray-100 hover:bg-gray-200 ">
+        <Popover placement="bottomRight" content={topLangsDom} trigger="click" open={open} onOpenChange={handleOpenChange}>
+            <div className="cursor-pointer  rounded p-1 px-2  ">
                 <IconTranslate className="stroke-1" />
             </div>
         </Popover>
